52e2091f3283aec68eddc5c4543c1d53fbe4e6d3
dschimpf@redhat.com <dds@redhat.com>
2023-04-27 17:17:25 -0400

    Automating apache config creation

--- setup.sh
+++ setup.sh
@@ -3,5 +3,11 @@ git clone https://github.com/purefield/opc-virt.git ~/demo
 ssh-keygen -N ""  -f ~/demo/demo.id_rsa
 echo "export subscriptionOrg=???
 export subscriptionKey=???" > ~/demo/subscription.txt
+for name in es-master00 es-master01 es-master02 coordinate elasticsearch kibana; do
+echo "" > ~/demo/demo.redhat.com/demo.conf
+  cat ~/demo/demo.redhat.com/demo.conf.template | \
+    perl -pe "s/\{\{ guid \}\}/$GUID/g" | \
+    perl -pe "s/\{\{ name \}\}/$name/g" >> ~/demo/demo.redhat.com/demo.conf
+done
 sudo cp ~/demo/demo.redhat.com/demo.conf /etc/httpd/conf.d/demo.conf
 sudo systemctl reload httpd
