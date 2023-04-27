7190c396f45bd32068aff68d2e0b630426b70195
dschimpf@redhat.com <dds@redhat.com>
2023-04-27 16:54:07 -0400

    Add apache config to setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ git clone https://github.com/purefield/opc-virt.git ~/demo
 ssh-keygen -N ""  -f ~/demo/demo.id_rsa
 echo "export subscriptionOrg=???
 export subscriptionKey=???" > ~/demo/subscription.txt
-
+sudo cp ~/demo/demo.redhat.com/demo.conf /etc/httpd/conf.d/demo.conf
+sudo sytemctl reload httpd
