1c0eef80e47d207535261fe286416025ddc85310
dschimpf@redhat.com <dds@redhat.com>
2023-04-28 13:42:12 -0400

    Adding letsencrypt cert

--- setup.sh
+++ setup.sh
@@ -3,5 +3,14 @@ git clone https://github.com/purefield/opc-virt.git ~/demo
 ssh-keygen -N ""  -f ~/demo/demo.id_rsa
 echo "export subscriptionOrg=???
 export subscriptionKey=???" > ~/demo/subscription.txt
+perl -pe "s/\{\{ guid \}\}/$GUID/g" ~/demo/demo.redhat.com/letsencrypt.conf.template | sudo tee /etc/httpd/conf.d/letsencrypt.conf > /dev/null
+sudo systemctl reload httpd
+sudo letsencrypt certonly --apache --agree-tos --email email@example.com \
+  -d elasticsearch.apps.$GUID.dynamic.opentlc.com \
+  -d coordinate.apps.$GUID.dynamic.opentlc.com \
+  -d kibanan.apps.$GUID.dynamic.opentlc.com \
+  -d es-master00.apps.$GUID.dynamic.opentlc.com \
+  -d es-master01.apps.$GUID.dynamic.opentlc.com \
+  -d es-master02.apps.$GUID.dynamic.opentlc.com
 perl -pe "s/\{\{ guid \}\}/$GUID/g" ~/demo/demo.redhat.com/demo.conf.template | sudo tee /etc/httpd/conf.d/demo.conf > /dev/null
 sudo systemctl reload httpd
