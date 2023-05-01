727f21df2c3711fa12629d92324e84351b054645
dschimpf@redhat.com <dds@redhat.com>
2023-05-01 15:13:53 -0400

    Prepare certs for OCP

--- elasticsearch/generate-yaml.sh
+++ elasticsearch/generate-yaml.sh
@@ -17,6 +17,10 @@ if [ -n "$GUID" ]
 then
   altDomain=$baseDomain
   baseDomain="apps.$GUID.dynamic.opentlc.com"
+  oc create secret tls letsencrypt \
+    --cert=~/demo/demo.redhat.com/cert.pem \
+    --key=~/demo/demo.redhat.com/privkey.pem \
+  -n $namespace --dry-run=client -o yaml >> $namespace.yaml
 fi
 cat namespace.yaml.template | perl -pe "s/\{\{ namespace \}\}/$namespace/g" > $namespace.yaml
 echo "---" >> $namespace.yaml
