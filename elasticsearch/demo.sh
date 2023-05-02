59ff9307f588433e3b6abaebf2644a5104f95503
dschimpf@redhat.com <dds@redhat.com>
2023-05-02 12:55:19 -0400

    Refine demo script output

--- elasticsearch/demo.sh
+++ elasticsearch/demo.sh
@@ -1,8 +1,20 @@
 export OK='\033[0;32mOK\033[0m'
+export ERROR='\033[0;31mERROR\033[0m'
 baseDomain=$(oc get --namespace openshift-ingress-operator ingresscontrollers/default -o jsonpath='{.status.domain}')
-cmd="curl -sk https://coordinate.$baseDomain/_cat/nodes"
+cmd="curl -sk https://elasticsearch.$baseDomain/_cat/nodes"
 echo
 echo "$cmd"
 echo
-echo -e "[ $OK ] Coordinate container is part of the VM cluster:" 
-$cmd | grep coordinate -B10 -A10 --color=always
+out=$($cmd 2>&1)
+if [ $? -eq 0 ]; then
+  if [ "x$(echo "$out" | grep coordinate)" == "x" ]; then
+    echo -e "[ $ERROR] VM cluster is up. Waiting on container" 
+    echo "$out" 
+  else
+    echo -e "[ $OK    ] Coordinate container is part of the VM cluster:" 
+    echo "$out" | grep coordinate -B10 -A10 --color=always
+  fi
+else
+  printf "[ $ERROR ] Not ready yet.\n"
+fi
+echo
