adcbb30ce517ec67553649421baad2a8a7f98bca
dschimpf@redhat.com <dds@redhat.com>
2023-05-16 14:52:34 -0400

    Adding script to build the app locally

---
+++ generate-data/rebuild.sh
@@ -0,0 +1,4 @@
+host=$(oc get route -l app=elasticsearch -l role=api -A -o jsonpath='{.items[].status.ingress[].host}')
+podman kill generate-data
+podman build -t generate-data .
+podman run -it --rm -p 3000:3000 -e ES_NODE=$host:443 -e DATA_RATE=100 --name generate-data generate-data:latest
