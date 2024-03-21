24c172be044edf8dfa98fbc11bb34ca8fac18cbc
Daniel Schimpfoessl <dds@redhat.com>
2024-03-21 14:25:29 -0500

    Adding data-view

---
+++ elasticsearch/kibana.data-view.sh
@@ -0,0 +1,9 @@
+curl -kX POST "https://$(oc get route -l app=kibana -A -ojsonpath='{.items[0].spec.host}')/api/data_views/data_view" -H "kbn-xsrf: true" -H "Content-Type: application/json" -d '
+{
+  "data_view": {
+		"name":"demo",
+    "title": "generated*",
+    "timeFieldName": "timestamp"
+  }
+}'
+
