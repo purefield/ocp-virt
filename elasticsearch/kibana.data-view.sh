4c258736d7d61b03497f4e51c0aa9b095a3c2b85
Daniel Schimpfoessl <dds@redhat.com>
2024-03-21 14:28:40 -0500

    Whitespace

--- elasticsearch/kibana.data-view.sh
+++ elasticsearch/kibana.data-view.sh
@@ -1,7 +1,7 @@
 curl -kX POST "https://$(oc get route -l app=kibana -A -ojsonpath='{.items[0].spec.host}')/api/data_views/data_view" -H "kbn-xsrf: true" -H "Content-Type: application/json" -d '
 {
   "data_view": {
-		"name":"demo",
+    "name":"demo",
     "title": "generated*",
     "timeFieldName": "timestamp"
   }
