23d95458d33ab84e41bb865a950a248bbabc6b2e
Daniel Schimpfoessl <dds@redhat.com>
2025-01-27 17:12:35 -0600

    Cleanup archive

--- archive/links/insert-sample-data.sh
+++
@@ -1,4 +0,0 @@
-export host=$(oc get route -l app=elasticsearch -l role=coordinate -A -o jsonpath='{.items[].status.ingress[].host}')
-echo "Connect to: $host"
-./mappings.curl $host
-./sample-data.curl $host
--- archive/links/src/main/java/com/redhat/links/Pattern.java
+++
@@ -1,5 +0,0 @@
-package com.redhat.links;
-
-public @interface Pattern {
-
-}
