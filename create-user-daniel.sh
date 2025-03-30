56710f72b38e93299c396df5efe1ce0c8477ef98
Daniel Schimpfoessl <dds@redhat.com>
2025-03-30 16:07:36 -0500

    Updating password for daniel

--- create-user-daniel.sh
+++ create-user-daniel.sh
@@ -1,6 +1,6 @@
 . ./format/format.sh
 __ "Create htpasswd secret" 4
-cmd "oc create secret generic htpasswd -n openshift-config --from-literal htpasswd='daniel:$apr1$wX6yA8lQ$jTj5rr3n6pNQg3vuee9qQ.'"
+cmd "oc create secret generic htpasswd -n openshift-config --from-literal htpasswd='daniel:$2y$05$GQlPEZ03BPQ4ZzkPD4dhKeM54VjDPN9YtyORDRVT6w.kU1p4Y4ZZ.'"
 
 __ "Create htpasswd provider" 4
 patch='[{"op":"add","path":"/spec/identityProviders/-","value":{"htpasswd":{"fileData":{"name":"htpasswd"}},"mappingMethod":"claim","name":"htpasswd","type":"HTPasswd"}}]'
